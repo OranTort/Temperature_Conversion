d4771afa2d88680dfe75bf0be7c9b3b9e86649c8
Rich <HONGRICHARD89@GMAIL.COM>
2025-06-08 21:58:37 -0700

    change pause to do while loop

--- temperature_conversion.cpp
+++ temperature_conversion.cpp
@@ -6,11 +6,12 @@ int main()
 {
     double temp;
     char unit;
+    char cont;
 
     std::cout << "***** Temperature Conversion ******" << '\n';
     std::cout << "F = Fahrenheit" << '\n';
     std::cout << "C = Celsius" << '\n';
-    std::cout << "What unit would you like to convert to?: ";
+    do{std::cout << "What unit would you like to convert to?: " << '\n';
     std::cin >> unit;
 
     if(unit == 'F' || unit == 'f'){
@@ -19,6 +20,8 @@ int main()
 
         temp = (1.8 * temp) + 32.0;
         std::cout << "The temperature is " << temp << " degrees Fahrenheit." << '\n';
+        std::cout << "Would you like to continue?(Y/N): " << '\n';
+        std::cin >> cont;
     }
     else if(unit == 'C' || unit == 'c'){
         std::cout << "Enter the temperature in Fahrenheit: " << '\n';
@@ -27,13 +30,16 @@ int main()
         
         temp = (temp - 32.0) / 1.8;
         std::cout << "The temperature is " << temp << " degrees Celsius." << '\n';
+        std::cout << "Would you like to continue?(Y/N): " << '\n';
+        std::cin >> cont;
     }
     else{
         std::cout << "Invalid response. Please select either Fahrenheit(F) or Celsius(C)." << '\n';
-    }
+        std::cout << "Would you like to continue?(Y/N): " << '\n';
+        std::cin >> cont;
+    }}while(cont == 'Y' || cont == 'y');
 
     std::cout << "***********************************" << '\n';
 
-    system("pause");
     return 0;
 }
